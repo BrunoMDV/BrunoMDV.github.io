3fcbaa9439816e340bdd6e3d1dd46ae7285fddee
PZeras <paulomirsouza80@gmail.com>
2023-11-17 22:47:42 -0300

    PaginaPaulinho

---
+++ index.js
@@ -0,0 +1,8 @@
+function chamarPaginaPaulinho(){
+    document.getElementById("paginaPaulinho").classList.remove("hide")
+    document.getElementById("paginaInicial").classList.add("hide")
+}
+function chamarPaginaInicial(){
+    document.getElementById("paginaPaulinho").classList.add("hide")
+    document.getElementById("paginaInicial").classList.remove("hide")
+}
